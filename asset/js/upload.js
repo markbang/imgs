6ec7c363dd4fcb2fa68739d2271756da87fad935
Bangwu <119404476+markbang@users.noreply.github.com>
2024-05-20 14:44:10 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -214,7 +214,7 @@ function render_uploaded() {
         var resp = x.resp;
         var name = resp.data.url.split('/').pop();
         
-        const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
+        const WPRAW = "https://i1.wp.com/upload.bangwu.top"  //使用wordpress.com加速
         const PhRAW = "https://telegra.ph"   //原地址
         const PROXYURL = ""  //自定义加速域名 默认是使用cloudflare
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
